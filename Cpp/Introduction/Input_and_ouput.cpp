a9ebd344abd20745aa1589924814cc8b7e52c2d9
Nikos Ar <nikos_rvnt@protonmail.com>
2018-03-13 21:27:25 +0200

    Create Input_and_ouput.cpp

---
+++ Cpp/Introduction/Input_and_ouput.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    
+    int in1, in2, in3, coutOUT ;
+    int sum = 0 ;
+    cin >> in1 >> in2 >> in3  ;
+    
+    //for(int i=0; cinIN[i];i++)
+    //{
+        sum = in1 + in2 + in3 ;
+    //}
+    cout << sum << endl ;
+    
+    return 0;
+}
