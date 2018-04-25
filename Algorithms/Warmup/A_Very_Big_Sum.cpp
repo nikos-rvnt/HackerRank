f59bda02d93624e9f89af75bf02cd936cb3b659d
Nikos Ar <nikos_rvnt@protonmail.com>
2018-04-25 23:06:40 +0300

    Create A_Very_Big_Sum.cpp

---
+++ Algorithms/Warmup/A_Very_Big_Sum.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long aVeryBigSum(int n, vector <long> ar) 
+{
+    // Complete this function
+    long biggy = 0 ;
+    for(int nu=0; nu<ar.size(); nu++)
+    {
+        biggy = biggy + ar[nu] ;
+    }
+    
+    return biggy ;
+    
+}
+
+int main() 
+{
+    int n ;
+    cin >> n ;
+
+    vector<long> ar(n) ;
+    for(int ar_i = 0; ar_i < n; ar_i++)
+    {
+       cin >> ar[ar_i] ;
+    }
+    long result = aVeryBigSum(n, ar) ;
+    
+    cout << result << endl ;
+    
+    return 0 ;
+    
+}
