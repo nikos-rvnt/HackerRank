001c3e219bb88474fc4df06e7326c2a3159401d8
Nikos Ar <nikos_rvnt@protonmail.com>
2018-05-29 02:18:01 +0300

    Create Java_Generics.java

---
+++ Java/Data_Structures/Java_Generics.java
@@ -0,0 +1,11 @@
+class Printer
+{
+   //Write your code here
+    public static <T> void printArray(T[] inputArray){
+        
+        for(T t : inputArray) {
+            System.out.println(t);
+        }
+    }
+ 
+}
