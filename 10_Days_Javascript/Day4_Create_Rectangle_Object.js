38f6d3bef6efd1aedefa39a0d6f2d16cbfcc5a19
Nikos Ar <nikos_rvnt@protonmail.com>
2018-05-29 00:06:14 +0300

    Create Day4_Create_Rectangle_Object.js

---
+++ 10_Days_Javascript/Day4_Create_Rectangle_Object.js
@@ -0,0 +1,48 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    var obj = {};
+    obj.length = a;
+    obj.width = b;
+    obj.perimeter = 2*(a+b);
+    obj.area = a*b;
+    //var obj = {a,b,2*(a+b),a*b};
+    return obj;
+}
+
+function main() {
+    const a = +(readLine());
+    const b = +(readLine());
+    
+    const rec = new Rectangle(a, b);
+    
+    console.log(rec.length);
+    console.log(rec.width);
+    console.log(rec.perimeter);
+    console.log(rec.area);
+}
