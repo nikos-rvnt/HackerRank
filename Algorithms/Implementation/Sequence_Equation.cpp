ba1fa8a7ed542256f015617c68b72b054c9216e8
Nikos Ar <nikos_rvnt@protonmail.com>
2018-05-31 16:54:59 +0300

    Update Sequence_Equation.cpp

--- Algorithms/Implementation/Sequence_Equation.cpp
+++ Algorithms/Implementation/Sequence_Equation.cpp
@@ -21,46 +21,31 @@
 #include <algorithm>
 #include <unordered_map>
 
-using namespace std;
+using namespace std ;
 
+int main() {
 
-int main(){
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int num ;
+    cin >> num ;
     
-    int n ;
-    int k ;
-    int q ;
-    cin >> n >> k >> q ;
-    
-    vector<int> a(n) ;
-    int temp ;
-    for(int a_i = 0; a_i < n; a_i++){
-       cin >> temp ;
-       if((a_i+k)<n)
-           a[a_i+k] = temp ;
-       else
-           a[((a_i+k)-n)%n] = temp ;     
+    vector<int> nums(num) ;
+    for(int n_i=0; n_i<num; n_i++){
+        cin >> nums[n_i] ;
     }
-    /*
-    int temp1, temp2 ;
-    for(int k_i=0; k_i<k; k_i++){
-        temp2 = a[0] ;
-        //int temp1 ;
-        for(int a_i=0; a_i<n; a_i++){
-            //if(a_i!=(n-1)){
-                temp1 = a[(a_i+1)%(n)] ;
-                a[(a_i+1)%n] = temp2 ;
-                temp2 = temp1 ; 
-            //}else{
-            //    a[0] = temp2 ;
-            //}
+    
+    for(int n_i=1; n_i<=num; n_i++){
+               
+        for(int n2=0; n2<num; n2++){
+            if(n_i==nums[n2]){
+                for(int n3=0; n3<num; n3++){
+                    if(nums[n2]==nums[nums[n3]-1])
+                        cout << n3+1 << endl ;
+                }
+            }
         }
-    }*/
-    
-    for(int a0 = 0; a0 < q; a0++){
-        int m ;
-        cin >> m ;
-        cout << a[m] << endl ;
     }
+    
     
     return 0 ;
     
