903cd6eb689d13ed6617193ecbf3b631595d8384
Nikos Ar <nikos_rvnt@protonmail.com>
2018-05-31 12:45:57 +0300

    Create Getting_Started_With_Conditionals.sh

---
+++ Linux_Shell/Bash/Getting_Started_With_Conditionals.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read inp 
+
+
+if  [ $inp == "y" ]; then
+  echo "YES"
+elif [ $inp == "Y" ]; then
+  echo "YES"
+elif [ $inp == "n" ]; then
+  echo "NO"
+elif [ $inp == "N" ]; then
+  echo "NO"
+fi
